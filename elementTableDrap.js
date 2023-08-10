83ad06c13b0f3ddaaadff41ba3c1897e1d43af8c
李嘉欣 <1006084794@qq.com>
2023-08-10 11:47:12 +0800

    修复bug

--- elementTableDrap.js
+++ elementTableDrap.js
@@ -20,6 +20,11 @@ export const moveTableCol = (filters = [], ref, data, callBack) => {
   }
   const priveLength = filters.filter((value) => value >= 0).length
   const header = ref.querySelector('table.el-table__header').querySelectorAll('th .cell')
+  // const tds = ref.querySelectorAll('tr')
+  // for (const td of tds) {
+  //   td.classList.add('animate__fadeIn')
+  //   td.classList.add('animate__animated')
+  // }
   for (const colKey in header) {
     let isJmp = false
     for (const index of filters) {
@@ -29,7 +34,7 @@ export const moveTableCol = (filters = [], ref, data, callBack) => {
         }
       } else {
         if (parseInt(colKey) === (header.length + index)) {
-          console.log(colKey, header.length, index, '跳出')
+          // console.log(colKey, header.length, index, '跳出')
           isJmp = true
         }
       }
@@ -43,7 +48,14 @@ export const moveTableCol = (filters = [], ref, data, callBack) => {
     }
     // console.log(headerCol , '123')
     headerCol.setAttribute('drapKey', (parseInt(colKey) - priveLength.toString()))
-    headerCol.draggable = true
+    // 增加拖拽触发方式
+    headerCol.addEventListener('mousedown', () => {
+      // 父元素不处于拖拽边框线下的状态，才能够拖拽
+      headerCol.draggable = (~event.target.parentElement.className.indexOf('noclick') && true)
+    })
+    headerCol.addEventListener('mouseup', () => {
+      headerCol.draggable = false
+    })
     const dragoverBack = (e) => {
       e.preventDefault()
       e.stopPropagation()
@@ -56,6 +68,8 @@ export const moveTableCol = (filters = [], ref, data, callBack) => {
       start.x = e.offsetX
       start.y = e.offsetY
       start.dom = e.target
+      // start.dom.parentElement.classList.add('trans-d')
+      // start.dom.parentElement.classList.add('trans-l')
     }
     headerCol.removeEventListener('dragstart', dragstartBack)
     headerCol.addEventListener('dragstart', dragstartBack, false)
@@ -72,20 +86,33 @@ export const moveTableCol = (filters = [], ref, data, callBack) => {
       const endkeyValue = data[endKey]
       // 判断是否为真，必填发生错误拖拽
       if (startKey && endKey) {
+        // console.log(start.dom, '123')
+        // end.dom.style.transform = `translateX(-${start.dom.scrollWidth}px)`
+        // start.dom.style.transform = `translateX(${end.dom.scrollWidth}px)`
         end.dom.setAttribute('drapKey', startKey)
         start.dom.setAttribute('drapKey', endKey)
         data[startKey] = endkeyValue
         data[endKey] = startkeyValue
+        start.dom.parentElement.classList.add('trans-d')
+        start.dom.parentElement.classList.add('trans-l')
+        // end.dom.parentElement.classList.add('trans-d')
+        // end.dom.parentElement.classList.add('trans-r')
         if (typeof callBack === 'function') {
           callBack()
         }
+        setTimeout(() => {
+          start.dom.parentElement.classList.remove('trans-d')
+          start.dom.parentElement.classList.remove('trans-l')
+          // end.dom.parentElement.classList.remove('trans-d')
+          // end.dom.parentElement.classList.remove('trans-r')
+        }, 100)
       }
       // console.log(startkeyValue, startKey)
       // console.log(endkeyValue, endKey)
     }
     headerCol.removeEventListener('dragenter', dropBack)
     headerCol.addEventListener('dragenter', dropBack, false)
-    // headerCol.removeEventListener('drag', dropBack)
-    // headerCol.addEventListener('drag', dropBack, false)
+    // headerCol.removeEventListener('drop', dropBack)
+    // headerCol.addEventListener('drop', dropBack, false)
   }
 }
