7c1f4b3b8d988d3047bfcd5334f8c122398fedef
LiJiaXin <1006084794@qq.com>
2023-10-31 10:45:14 +0800

    增加渲染树的方法

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//   plugins: {
+//       'postcss-pxtorem': {
+//           // 多少像素为1rem（设计尺寸下的根字体大小）
+//           rootValue: 12,
+//           // 行内样式的px不会转换为rem
+//           propList: ['*']
+//       }
+//   }
+// }
