1876279dceab7dd9472e09388a354ac14f4b751f
雪小花 <lijiaxin@mtfxh.wiki>
2023-11-23 23:22:20 +0800

    添加加密加密模块

--- AE4.ts
+++ AE4.ts
@@ -1,23 +1,9 @@
 import CryptoJS from 'crypto-js';
-// 解密函数
-export function encryptData(data: string, keyWord: string) {
-  const key = CryptoJS.enc.Utf8.parse(keyWord);
-  const dataBytes = CryptoJS.enc.Utf8.parse(data);
-  const encrypted = CryptoJS.AES.encrypt(dataBytes, key, {
-    mode: CryptoJS.mode.ECB,
-    padding: CryptoJS.pad.Pkcs7
-  });
-  const encryptedData = encrypted.toString();
-  return encryptedData;
+// 定义加密函数
+export function encryptData(tex: string, key: number) {
+  return  CryptoJS.AES.encrypt(tex, key).toString()
 }
-// 加密函数
-export function decryptData(encryptedData: string, keyWord: string) {
-  const key = CryptoJS.enc.Utf8.parse(keyWord);
-  const encryptedBytes = CryptoJS.enc.Base64.parse(encryptedData);
-  const decrypted = CryptoJS.AES.decrypt(encryptedBytes, key, {
-    mode: CryptoJS.mode.ECB, // 加密模式
-    padding: CryptoJS.pad.Pkcs7 // 填充模式
-  });
-  const decryptedData = decrypted.toString(CryptoJS.enc.Utf8);
-  return decryptedData;
+// 定义解密函数
+export function decryptData(text: string, key: number) {
+  return CryptoJS.AES.decrypt(text, key).toString(CryptoJS.enc.Utf8)
 }
