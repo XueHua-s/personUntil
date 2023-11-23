63bb33dddd112e0253b8ecdf74e1aee7b4c2a976
雪小花 <lijiaxin@mtfxh.wiki>
2023-11-23 23:22:40 +0800

    1

--- AE4.ts
+++ AE4.ts
@@ -1,9 +1,9 @@
 import CryptoJS from 'crypto-js';
 // 定义加密函数
-export function encryptData(tex: string, key: number) {
+export function encryptData(tex: string, key: string) {
   return  CryptoJS.AES.encrypt(tex, key).toString()
 }
 // 定义解密函数
-export function decryptData(text: string, key: number) {
+export function decryptData(text: string, key: string) {
   return CryptoJS.AES.decrypt(text, key).toString(CryptoJS.enc.Utf8)
 }
